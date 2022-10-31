a3d43ef8c0f2792bec5d5b6429eb47278d04eb6e
Carl Brasic <cbrasic@gmail.com>
2022-10-30 23:16:06 -0500

    Add start/stop commands

--- exec.go
+++ exec.go
@@ -3,6 +3,7 @@ package launchd
 import (
 	"bytes"
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 )
@@ -17,18 +18,45 @@ func (s *Service) Bootstrap() ([]byte, error) {
 }
 
 // Bootout uninstalls a service (`launchctl bootout` née `launchctl unload`)
-func (s *Service) Bootout() error {
-	path, err := s.DefinitionPath()
+// Optionally deletes the service definition file (plist) as well.
+func (s *Service) Bootout(removePlist bool) error {
+	err := s.launchctlUnaryCommand("bootout")
 	if err != nil {
 		return err
 	}
-	_, err = s.launchctl("bootout", domain, path)
-	return err
+	if removePlist {
+		path, err := s.DefinitionPath()
+		if err != nil {
+			return fmt.Errorf("could not find definition path: %w", err)
+		}
+		err = os.Remove(path)
+		if err != nil {
+			return fmt.Errorf("could not remove definition: %w", err)
+		}
+	}
+	return nil
+}
+
+// Start a service (`launchctl start`)
+func (s *Service) Start() error {
+	return s.launchctlUnaryCommand("start")
+}
+
+// Stop a service (`launchctl stop`)
+func (s *Service) Stop() error {
+	return s.launchctlUnaryCommand("stop")
 }
 
 // Print service state (`launchctl print`)
 func (s *Service) Print() ([]byte, error) {
 	return s.launchctl("print", s.UserSpecifier())
+}
+
+// Run a launchctl(1) subcommand against a service with the only argument being
+// the service name. Returns error if not successful.
+func (s *Service) launchctlUnaryCommand(command string) error {
+	_, err := s.launchctl(command, s.UserSpecifier())
+	return err
 }
 
 // Run a launchctl(1) subcommand for the service and return the output or an error
